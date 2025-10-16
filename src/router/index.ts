46f561ecb17e5916b0b30e93349089866bae71ec
KSksip <->
2025-10-16 10:06:43 +0200

    renamed routes folder to pages for better organization

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 
-import Home from '../routes/Home.vue'
-import Projects from '../routes/Projects.vue'
+import Home from '../pages/Home.vue'
+import Projects from '../pages/Projects.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
