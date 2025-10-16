083d2ccbd0c7727862001fdd1faf33fcc7152bfb
KSksip <->
2025-10-16 10:01:40 +0200

    misc stuff added footer and nav titles

--- src/App.vue
+++ src/App.vue
@@ -17,22 +17,33 @@ function toggleDarkMode(){
   }
 }
 
+
 </script>
 
 <template>
-  <div class="animate-fade-in">
+  <div class="animate-fade-in min-h-screen flex flex-col justify-between">
     <div class="font-IBMPlexMono sticky top-0">
       <nav class="font-adwaita flex dark:bg-zinc-800/90 bg-white/90 grow-0 flex-row border-b-1 dark:border-zinc-700 border-zinc-200 p-2 justify-center h-13 text-2xl 
-      [&_div]:w-1/3 [&_div]:flex">
-          <div>
+      [&_div]:w-1/3 [&_div]:flex
+      [&_div>*]:hover:text-accent dark:[&_div>*]:hover:text-accent-2">
+          <div class="">
               <a href="https://github.com/KSksip" class="flex gap-2">
                 <v-icon class="size-8" name="fa-github" />
                 <h2 class="font-bold">KSksip</h2>
               </a>
           </div>
-          <div class="justify-center gap-6">
-              <!--<router-link to="/">Home</router-link>
-              <router-link to="/projects">Projects</router-link>-->
+          <div class="justify-center *:font-medium gap-6">
+              <router-link 
+                to="/" 
+                :class="$route.fullPath == '/' ? 'text-accent dark:text-accent-2' : ''">
+                Home
+              </router-link>
+
+              <router-link 
+                to="/projects"
+                :class="$route.fullPath == '/projects' ? 'text-accent dark:text-accent-2' : ''">
+                Projects
+              </router-link>
           </div>
 
           <div class="justify-end pe-2">
@@ -46,8 +57,12 @@ function toggleDarkMode(){
       </nav>
     </div>
     
-    <div class="py-8 cursor-default md:w-200 mx-auto">
+    <div class="py-8 cursor-default md:w-200 mx-auto grow">
       <RouterView />
+    </div>
+
+    <div class="bg-zinc-100/20 inset-shadow-sm flex gap-2 justify-evenly p-6 mt-10 *:text-zinc-600">
+      <a href="https://github.com/KSksip/KSksip.github.io" class="hover:text-accent hover:underline">See Page Source</a>
     </div>
   </div>
 </template>
